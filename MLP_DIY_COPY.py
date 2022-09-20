19c8ea210cc6a8cddaa305a65aa9dd2c1068b027
linkedlist771 <72634327+linkedlist771@users.noreply.github.com>
2022-09-21 00:15:03 +0800

    实现了梯度的保存，实现了激活函数的类
    实现了梯度的保存，实现了激活函数的类

--- MLP_DIY_COPY.py
+++ MLP_DIY_COPY.py
@@ -1,61 +1,73 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
-import traceback
 
-
-def sigmoid(x):
+def sigmoid(X):
     '''
     sigmoid 激活函数
-    :param x: 输入
+    :param X: 输入
     :return:  输出
     '''
 
-    return 1/(1+np.exp(-x))
+    return 1/(1+np.exp(-X))
 
 
-def tanh(x):
+def tanh(X):
     '''
     tanh 激活函数
-    :param x: 输入
+    :param X: 输入
     :return:  输出
     '''
 
-    return np.tanh(x)
+    return np.tanh(X)
 
 
 
-def relu(x):
+def relu(X):
     '''
     relu 激活函数
-    :param x: 输入
+    :param X: 输入
     :return:  输出
     '''
-    a = np.copy(x)
-    a[np.where(a <= 0)] = 0
+    a = np.copy(X)
+    a[np.where(a<=0)] = 0
     return a
 
 
-def leaky_relu(x, r=0.01):
+def leaky_relu(X, r=0.01):
     '''
     LeakyRelu激活函数
-    :param x: 输入
+    :param X: 输入
     :return:  输出
     '''
-    a = np.copy(x)
-    a[np.where(a <= 0)] *= r
+    a = np.copy(X)
+    a[np.where(a<=0)] *= r
     return a
 
 
-def e_relu(x, r=0.01):
+def e_relu(X, r=0.01):
     '''
     LeakyRelu激活函数
-    :param x: 输入
+    :param X: 输入
     :return:  输出
     '''
-    a = np.copy(x)
-    a[np.where(a <= 0)] = r*(np.exp(a[np.where(a <= 0)])-1)
+    a = np.copy(X)
+    a[np.where(a<=0)] = r*(np.exp(a[np.where(a<=0)])-1)
     return a
+
+
+def no_activate(X):
+    '''
+
+    Parameters
+    ----------
+    X:输入
+
+    Returns
+    -------
+
+    '''
+    return X
 
 
 class MLP():
@@ -71,22 +83,21 @@ class MLP():
         self.num_input = num_input
         self.num_hidden = num_hidden
         self.num_output = num_output
-        #self.W1 = np.random.random(size=(num_input, num_hidden))  # 第一层的权重
-        #self.b1 = np.zeros(shape=(1, num_hidden)) # 第一层偏置
-        #self.W2 = np.random.random(size=(num_hidden, num_output))  # 第二层的权重
-        #self.b2 = np.zeros(shape=(1, num_output)) # 第二层偏置
-        #self.theta = np.array([self.W1, self.b1, self.W2, self.b2])
+        self.W1 = np.random.random(size=(num_input, num_hidden))  # 第一层的权重
+        self.b1 = np.zeros(shape=(1, num_hidden)) # 第一层偏置
+        self.W2 = np.random.random(size=(num_hidden, num_output))  # 第二层的权重
+        self.b2 = np.zeros(shape=(1, num_output)) # 第二层偏置
+        self.theta = np.array([self.W1, self.b1, self.W2, self.b2])
         self.activation_function = relu # 定义激活函数
         self.params = {}
-        self.params['W1'] = np.random.random(size=(num_input, num_hidden))  # 第一层的权重
-        self.params['b1'] = np.zeros(shape=(1, num_hidden))  # 第一层偏置
-        self.params['W2'] = np.random.random(size=(num_hidden, num_output))  # 第二层的权重
-        self.params['b2'] = np.zeros(shape=(1, num_output))  # 第二层偏置
-        self.theta = np.array([self.params['W1'], self.params['b1'], self.params['W2'], self.params['b2']], dtype=object)
+        self.params['W1'] = self.W1
+        self.params['b1'] = self.b1
+        self.params['W2'] = self.W2
+        self.params['b2'] = self.b2
     def forward(self, X):
         X = np.reshape(X,(-1, self.num_input))
-        H = self.activation_function(np.dot(X, self.params['W1'])+self.params['b1'])
-        return  np.dot(H, self.params['W2'])+ self.params['b2'] # 返回这个值
+        H = self.activation_function(np.dot(X, self.W1)+self.b1)
+        return  np.dot(H, self.W2)+self.b2 # 返回这个值
 
 
     def predict(self, X, theta):
@@ -124,8 +135,6 @@ class MLP():
 
 
 
-
-
     def sgd(self, X, y, delta = 1e-6 , lr=0.0001):
         '''
         用于做SGD优化的权重以及偏置参数即为__init__
@@ -157,11 +166,11 @@ class MLP():
         for index in range(len(theta)):
             para = theta[index]
             grad_t = np.zeros(shape=para.shape)  # 获得存储这个梯度的列表
-            for i in range(len(grad_t.flatten())):
-                delta_theta = np.array([np.zeros(shape = self.params['W1'].shape),
-                                        np.zeros(shape = self.params['b1'].shape),
-                                        np.zeros(shape = self.params['W2'].shape),
-                                        np.zeros(shape = self.params['b2'].shape)], dtype=object)  # 获得形状如X的值
+            for  i  in  range(len(grad_t.flatten())):
+                delta_theta = np.array([np.zeros(shape = self.W1.shape),
+                                        np.zeros(shape = self.b1.shape),
+                                        np.zeros(shape = self.W2.shape),
+                                        np.zeros(shape = self.b2.shape)])  # 获得形状如X的值
                 shape = delta_theta[index].shape
                 delta_theta_index = delta_theta[index].ravel()
                 delta_theta_index[i] = delta
@@ -173,13 +182,9 @@ class MLP():
                 self_theta = self.theta[index].ravel()
                 self_theta[i] -= grad*lr
                 self.theta[index] = self_theta.reshape(shape)
-        self.params['W1'] = self.theta[0]
-        self.params['b1'] = self.theta[1]
-        self.params['W2'] = self.theta[2]
-        self.params['b2'] = self.theta[3]
-
     def train(self):
         pass
+
 
     def print_weight(self):
         print("W1")
@@ -191,17 +196,25 @@ class MLP():
         print("b2")
         print(self.b2)
 
-    # 保存权重文件    
-    def save(self, path):
-        with open(path, 'wb') as f :
+
+    # 保存权重文件
+    def save(self,path):
+        with open(path,'wb') as f :
             pickle.dump(self.params, f)
 
     # 导入权重文件
-    def load(self, path):
-        with open(path, 'rb') as f :
+    def load(self,path):
+        with open(path,'rb') as f :
             self.params = pickle.load(f)
-            self.theta = np.array([self.params['W1'], self.params['b1'], self.params['W2'],
-                                   self.params['b2']], dtype=object)
+            self.W1 = self.params['W1']
+            self.b1 = self.params['b1']
+            self.W2 = self.params['W2']
+            self.b2 = self.params['b2']
+            self.theta = np.array([self.W1, self.b1, self.W2, self.b2])
+        #print(weight)
+        #print(self.params)
+
+
 
 
 if __name__ == '__main__':
@@ -209,39 +222,35 @@ if __name__ == '__main__':
     num_hidden = 30  # 设置隐藏层个数
     num_output = 1
     save_path = "weight.pickle"
-    train_net = False
-    load_weight = True
+    train_net = True
     # 尝试读取保存的权重
     X = np.arange(0, 2*np.pi, 0.1).reshape((-1, num_input))
-    y = np.sin(X)
+    y =  np.sin(X)
     model = MLP(num_input, num_hidden, num_output)
-    if load_weight:
-        try:
-            model.load(save_path) # 尝试读取权重
-        except (Exception, BaseException) as e:
-            print('{:*^60}'.format('直接打印出e, 输出错误具体原因'))
-            print(e)
-            print('{:*^60}'.format('使用repr打印出e, 带有错误类型'))
-            print(repr(e))
-            print('{:*^60}'.format('使用traceback的format_exc可以输出错误具体位置'))
-            exstr = traceback.format_exc()
-            print(exstr)
-    epoch = 1000000  # 设置100次训练
+    #model.load(save_path)  # 尝试读取权重
+
+    try:
+        pass #model.load(save_path) # 尝试读取权重
+    except:
+        pass
+    epoch =  1000000 # 设置100次训练
 
     if train_net:
-        for i in range(1, epoch+1):
-            model.sgd(X, y, lr=1e-4)
-            if i % 100 == 0:
+        for i in range(1,epoch+1):
+            model.sgd(X,y,lr=1e-3)
+            if i%100 == 0:
                 print(f"第{i}/{epoch}次训练")
                 print(f"训练误差{model.loss_function(y,model.forward(X))}")
                 model.save(save_path)
 
+    #print(f"开始的X:\n{X}")
+    #print(f"经过MLP前向传播的X:\n{model.forward(X)}")
     plt.figure()
     plt.scatter(X, y, color="black" )
     plt.plot(X, model.forward(X), color="red")
     plt.title("MLP fits sin by hands")
-    plt.legend(["original", "predicted"])
-    plt.savefig("手动实现_MLP.png", dpi=300)
+    plt.legend(["original","predicted"])
+    plt.savefig("手动实现_MLP.png",dpi=300)
     plt.show()
 
 
---
+++ activation_function.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+
+class Sigmoid:
+    def __init__(self, x):
+        self.x = x
+
+    def forward(self):
+        '''
+
+        Returns
+        -------
+        返回正向计算得到的值
+        '''
+
+        return 1/(1+np.exp(-self.x))
+    def backward(self):
+        '''
+
+        Returns
+        -------
+        返回sigmoid激活函数的求导后的值
+        '''
+
+        return self.forward()-np.square(self.forward())
+
+
+class TanH:
+    def __init__(self, x):
+        self.x = x
+
+    def forward(self):
+        return np.tanh(self.x)
+
+    def backward(self):
+        return np.square(np.sinh(self.x))
+
+
+class Relu:
+    def __init__(self, x):
+        self.x = x
+
+    def forward(self):
+        a = np.copy(self.x)
+        a[np.where(a <= 0)] = 0
+        return a
+
+    def backward(self):
+        a = np.zeros_like(self.x)
+        a[np.where(a > 0)] = 1
+        return a
+
+
+class LeakyRelu:
+    def __init__(self, x, r=0.01):
+        self.x = x
+        self.r = r
+
+    def forward(self):
+        a = np.copy(self.x)
+        a[np.where(a <= 0)] *= self.r
+        return a
+
+    def backward(self):
+        a = np.ones_like(self.x)
+        a[np.where(a <= 0)] = self.r
+        return a
+
+
+class ERelu:
+    def __init__(self, x, r=0.01):
+        self.x = x
+        self.r = r
+
+    def forward(self):
+        a = np.copy(self.x)
+        a[np.where(a <= 0)] = self.r * (np.exp(a[np.where(a <= 0)]) - 1)
+        return a
+
+    def backward(self):
+        a = np.ones_like(self.x)
+        a[np.where(a <= 0)] = self.r * np.exp(a[np.where(a <= 0)])
+        return a
+
